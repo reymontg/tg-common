632e5883bf815cd01f6bd367c26fa210fe7f947d
ahjdev <amirhosseinjafari8228@gmail.com>
2025-06-21 17:58:37 +0330

    fix cs-fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,8 +2,8 @@
 
 $config = new Amp\CodeStyle\Config;
 $config->getFinder()
-    ->in(__DIR__ . "/src")
-    ->in(__DIR__ . "/tests");
+    ->in(__DIR__ . "/src");
+    // ->in(__DIR__ . "/tests");
 
 $config->setCacheFile(__DIR__ . '/.php-cs-fixer.cache');
 
